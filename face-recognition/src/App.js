1f8ebf9b8f28d049d8415be75bac5dadb3f3deaa
Richie <richardokoye2@gmail.com>
2019-02-20 01:32:08 +0100

    added rank component

--- face-recognition/src/App.js
+++ face-recognition/src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+
+import Particles from 'react-particles-js';
+
+const particleOptions = {
+  particles: {
+     number: {
+       value: 70,
+       density:{
+        enable: true,
+        value_area: 800
+       }
+     }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className="particles" 
+                params={particleOptions} />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
       </div>
     );
